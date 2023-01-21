f4a37033b7f705bf4e5aa317a4f35f6ba3f473d3
Folasayo Owolabi <Phollyb4luv205@gmail.com>
2023-01-21 19:12:45 +0100

    Updating icon

--- src/app.js
+++ src/app.js
@@ -22,12 +22,14 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -37,6 +39,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "de879077b25c4821b4116348dcf1cdcc";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=${apiKey}&units=metric`;
+let city = "Ibadan";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
