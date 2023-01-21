0babb2c8f23a56bc391a7a77a6f8365a3d1b6858
Folasayo Owolabi <Phollyb4luv205@gmail.com>
2023-01-21 17:57:46 +0100

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "de879077b25c4821b4116348dcf1cdcc";
